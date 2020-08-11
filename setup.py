d9719c694a96dcb023e04c8279813a38a01f550a
ripll <ripll.1998@gmail.com>
2020-08-12 02:11:55 +0300

    Install requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='peregrinearb',
-    version='1.8.0',
+    version='1.8.1',
     description='A Python library which provides several algorithms to detect arbitrage opportunities across over 132 '
                 'cryptocurrency markets in over 50 countries',
     author='Ward Bradt',
     author_email='wardbradt5@gmail.com',
     packages=find_packages(),
+    install_requires=['git+git://github.com/wardbradt/networkx.git#egg=networkx',
+                      'numpy',
+                      'ccxt',
+                      'web3'],
     license='MIT',
     url='https://github.com/Stakedllc/peregrine/',
 )
